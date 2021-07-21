e2abe2c70d86b4591611294df7f3e5fccdf5e475
malekak44 <malekak44@gmail.com>
2021-07-21 17:24:56 +0600

    eid-ul-adha

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,16 +1,74 @@
 import React from 'react';
 import './Destination.css';
 import destination from '../../images/destination.webp';
+import map from '../../images/Map.png';
+import car from '../../images/car.png';
+import people from '../../images/peopleicon.png';
 
 const Destination = () => {
     const url = window.location.href;
     if (url.includes('destination')) {
         document.body.style.backgroundImage = `url(${destination})`;
     }
+
+    const showResult = (e) => {
+        e.preventDefault();
+        const mapForm = document.getElementById("mapForm");
+        mapForm.style.display = "none";
+        const resultDiv = document.getElementById("result-div");
+        resultDiv.style.display = "block";
+    }
     return (
-        <div>
+        <>
             <hr />
-        </div>
+            <div className="container destination">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div id="result-div">
+                            <div className="places">
+                                <p style={{ marginTop: "13px" }}>• Mirpur 1</p>
+                                <p>• Dhanmondi</p>
+                            </div>
+                            <div className="car">
+                                <img className="carIcon" src={car} alt="car" />
+                                <p className="carName">Car</p>
+                                <img className="peopleIcon" src={people} alt="people" />
+                                <span className="quantity">4</span>
+                                <p>$67</p>
+                            </div>
+                            <div className="car">
+                                <img className="carIcon" src={car} alt="car" />
+                                <p className="carName">Car</p>
+                                <img className="peopleIcon" src={people} alt="people" />
+                                <span className="quantity">4</span>
+                                <p>$67</p>
+                            </div>
+                            <div className="car">
+                                <img className="carIcon" src={car} alt="car" />
+                                <p className="carName">Car</p>
+                                <img className="peopleIcon" src={people} alt="people" />
+                                <span className="quantity">4</span>
+                                <p>$67</p>
+                            </div>
+                        </div>
+                        <form id="mapForm" onSubmit={showResult}>
+                            <div className="form-group">
+                                <label htmlFor="from">Pick From</label>
+                                <input type="text" className="form-control" id="from" value="Mirpur 1" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="to">Pick To</label>
+                                <input type="text" className="form-control" id="to" value="Dhanmondi" />
+                            </div>
+                            <button type="submit" className="btn">Submit</button>
+                        </form>
+                    </div>
+                    <div className="col-md-8">
+                        <img id="map" style={{ width: "97%" }} src={map} alt="map" />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import { useContext } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../App';
 import './Header.css';
 const Header = () => {
     const [signedInUser, setSignedInUser] = useContext(UserContext);
     let loginStatus;
+    let brand = document.getElementById("brand");
     if (signedInUser.name) {
-        loginStatus = <><li className="nav-item"><p style={{ width: "185px", textShadow: "0px 0px 10px white", color: "#FFAF24" }}>{signedInUser.name}</p></li><li className="nav-item"><button onClick={() => setSignedInUser({})} type="button">Signout</button></li></>
+        loginStatus = <><Nav.Link><p id="username" style={{ textShadow: "0px 0px 10px white", color: "#FFAF24" }}>{signedInUser.name}</p></Nav.Link><Nav.Link className="nav-item"><button onClick={() => setSignedInUser({})} type="button">Signout</button></Nav.Link></>
+        brand.style.marginTop = "-16px";
     } else {
-        loginStatus = <><li className="nav-item"><Link to="/login"><button type="button">Login</button></Link></li></>;
+        loginStatus = <><Nav.Link className="nav-item"><Link to="/login"><button type="button">Login</button></Link></Nav.Link></>;
+        if (brand)
+            brand.style.marginTop = "-2px";
     }
     return (
         <div className="container header">
-            <nav className="navbar navbar-expand-lg">
-                <h1 className="navbar-brand">Rapid Movers</h1>
-                <div className="container nav">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-                                <p>Home</p>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/destination" style={{ textDecoration: "none", color: "white" }}>
-                                <p>Destination</p>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <p>Contact</p>
-                        </li>
-                        {loginStatus}
-                    </ul>
-                </div>
-            </nav>
+            <Navbar expand="lg">
+                <Container>
+                    <Navbar.Brand id="brand">Rapid Movers</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link>
+                                <Link to="/" style={{ textDecoration: "none", color: "white" }}>Home</Link>
+                            </Nav.Link>
+                            <Nav.Link >
+                                <Link to="/destination" style={{ textDecoration: "none", color: "white" }}>Destination</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                Contact
+                            </Nav.Link>
+                            {loginStatus}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
